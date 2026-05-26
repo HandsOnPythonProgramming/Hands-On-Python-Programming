cca0c2b572e6f335cc9b718686685fc2879a6e63
HandsOnPythonProgramming <freehpcuser@gmail.com>
2026-05-26 18:16:06 -0500

    Update quiz_12.py

--- Data/quiz_data/python_data/quiz_12.py
+++ Data/quiz_data/python_data/quiz_12.py
@@ -1,10 +1,10 @@
 import json
+import requests
 import gradio as gr
 
-QUIZ_PATH = "https://github.com/HandsOnPythonProgramming/Hands-On-Python-Programming/blob/c456a1c64983697bdbc6ab244c857d5724d50b6e/Data/quiz_data/json_data/quiz_12.json"
+QUIZ_URL = "https://raw.githubusercontent.com/HandsOnPythonProgramming/Hands-On-Python-Programming/main/Data/quiz_data/json_data/quiz_12.json"
 
-with open(QUIZ_PATH, "r") as f:
-    QUIZ = json.load(f)["quiz"]
+QUIZ = requests.get(QUIZ_URL).json()["quiz"]
 
 def load_question(index):
     q = QUIZ[index]
